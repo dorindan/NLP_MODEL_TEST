8f487c1b88a173eaf8499f5a845f5276dcf66efc
dorindan <gabudeandorin@yahoo.ro>
2021-05-25 16:36:42 +0300

    yes

--- src/main/java/smart/smartbook/controller/WholeBookController.java
+++ src/main/java/smart/smartbook/controller/WholeBookController.java
@@ -23,9 +23,9 @@ public class WholeBookController {
         return wholeBookService.getBookById(id).get();
     }
 
-    @RequestMapping(value = "/{author}/{bookname}/{bookpath}", method= RequestMethod.POST)
-    public WholeBook getEventUsers(@PathVariable String author, @PathVariable String bookname, @PathVariable String bookpath){
-        return wholeBookService.saveWholeBook(author, bookname, bookpath);
+    @RequestMapping(value = "/save/{author}/{bookname}/{bookpathName}", method= RequestMethod.POST)
+    public WholeBook getEventUsers(@PathVariable String author, @PathVariable String bookname, @PathVariable String bookpathName){
+        return wholeBookService.saveWholeBook(author, bookname, bookpathName);
     }
 
     @RequestMapping(value = "/bookname/{bookname}", method= RequestMethod.GET)
@@ -37,4 +37,33 @@ public class WholeBookController {
     public ExecutionStatus getPeopleNER(@PathVariable String bookname) throws IOException {
         return wholeBookService.getPeopleNER(bookname);
     }
+
+    @RequestMapping(value = "/NER/date/{bookname}", method= RequestMethod.GET)
+    public ExecutionStatus getDateNER(@PathVariable String bookname) throws IOException {
+        return wholeBookService.getDateNER(bookname);
+    }
+
+    @RequestMapping(value = "/NER/location/{bookname}", method= RequestMethod.GET)
+    public ExecutionStatus getLocationNER(@PathVariable String bookname) throws IOException {
+        return wholeBookService.getLocationNER(bookname);
+    }
+//
+//    @RequestMapping(value = "/NER/money/{bookname}", method= RequestMethod.GET)
+//    public ExecutionStatus getMoneyNER(@PathVariable String bookname) throws IOException {
+//        return wholeBookService.getMoneyNER(bookname);
+//    }
+//
+//    @RequestMapping(value = "/NER/organization/{bookname}", method= RequestMethod.GET)
+//    public ExecutionStatus getOrganizationNER(@PathVariable String bookname) throws IOException {
+//        return wholeBookService.getOrganizationNER(bookname);
+//    }
+//    @RequestMapping(value = "/NER/percentage/{bookname}", method= RequestMethod.GET)
+//    public ExecutionStatus getPercentageNER(@PathVariable String bookname) throws IOException {
+//        return wholeBookService.getPercentageNER(bookname);
+//    }
+//    @RequestMapping(value = "/NER/time/{bookname}", method= RequestMethod.GET)
+//    public ExecutionStatus getTimeNER(@PathVariable String bookname) throws IOException {
+//        return wholeBookService.getTimeNER(bookname);
+//    }
+
 }
--- src/main/java/smart/smartbook/model/ExecutionStatus.java
+++ src/main/java/smart/smartbook/model/ExecutionStatus.java
@@ -3,6 +3,7 @@ package smart.smartbook.model;
 import lombok.Data;
 
 import java.io.InputStream;
+import java.util.List;
 
 @Data
 public class ExecutionStatus {
@@ -13,5 +14,32 @@ public class ExecutionStatus {
 
     private String model;
 
-    private Long executionTime;
+    private Long modelReadExecutionTime1;
+    private Long modelReadExecutionTime2;
+    private Long modelReadExecutionTime3;
+    private Long modelReadExecutionTime4;
+    private Long modelReadExecutionTime5;
+    private Long modelReadExecutionTime6;
+    private Long modelReadExecutionTime7;
+    private Long modelReadExecutionTime8;
+    private Long modelReadExecutionTime9;
+    private Long modelReadExecutionTime10;
+
+    private Double modelReadExecutionTimeAverage;
+
+    private Long modelprocessExecutionTime1;
+    private Long modelprocessExecutionTime2;
+    private Long modelprocessExecutionTime3;
+    private Long modelprocessExecutionTime4;
+    private Long modelprocessExecutionTime5;
+    private Long modelprocessExecutionTime6;
+    private Long modelprocessExecutionTime7;
+    private Long modelprocessExecutionTime8;
+    private Long modelprocessExecutionTime9;
+    private Long modelprocessExecutionTime10;
+
+    private Double modelprocessExecutionTimeAverage;
+
+    private List<String> results;
+
 }
